81e725870a646f51a3881334f420d1b6c6cf495c
Sven <sven@legalthings.io>
2018-04-12 14:38:47 +0200

    Allow json parsing (#1)
    * Add ability to parse json to csv as visa versa
    
    * Add travis conf
    
    * Fix tests

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
 'use strict';
+const Parser = require('./lib/parser');
 
-const parse = require('csv-parse');
-const request = require('request-promise');
 const winston = require('winston');
 const logger = new (winston.Logger)({
   transports: [ new winston.transports.Console() ]
 });
 
-exports.handler = (event, context, callback) => {
-  if (!event || !event.url) {
-    return callback(`no 'url' property given`);
+exports.handler = async (event) => {
+  if (!event || !event.url && !event.content) {
+    throw(`no 'url' or 'content' property given`);
   }
 
-  request(event.url).then((data) => {
-    parse(data, { columns: true }, (err, parsed) => {
-      if (err) {
-        return callback(`failed to parse csv file: ${err}`);
-      }
-     
-      callback(null, parsed);
-    })
-  }).catch((err) => {
-    return callback(`failed to download csv file: ${err}`);
-  });
+  const parser = new Parser();
+  return await parser.parseEvent(event);
 };
--- tests/index.js
+++ tests/index.js
@@ -3,12 +3,13 @@
 const lambdaLocal = require('lambda-local');
 const nock = require('nock');
 const should = require('should');
+const path = require('path');
 
 process.env.NODE_ENV = 'tests';
 
 describe('index', () => {
   describe('handler', () => {
-    it('should return parsed csv contents by fetching csv from url', () => {
+    it('should return json contents by fetching csv from url', () => {
       nock('http://example.com', { allowUnmocked: true })
         .get(`/input.csv`)
         .reply(200, [
@@ -29,6 +30,111 @@ describe('index', () => {
           { "name": "jane", "email": "jane@example.com" },
           { "name": "bob ross", "email": "bob@example.com" }
         ]);
+      }).catch((err) => {
+        should(err).be.null;
+      });
+    });
+
+    it('should return csv contents by fetching json from url', () => {
+      nock('http://example.com', { allowUnmocked: true })
+        .get(`/input.json`)
+        .reply(200, [
+          { "name": "john", "email": "john@example.com" },
+          { "name": "jane", "email": "jane@example.com" },
+          { "name": "bob ross", "email": "bob@example.com" }
+        ]);
+
+      lambdaLocal.execute({
+        event: {
+          url: 'http://example.com/input.json'
+        },
+        lambdaPath: path.join(__dirname, '../index.js'),
+        profileName: 'default',
+        timeoutMs: 3000
+      }).then((done) => {
+        should(done).eql([
+          'name,email',
+          'john,john@example.com',
+          'jane,jane@example.com',
+          'bob ross,bob@example.com'
+        ].join('\n'));
+      }).catch((err) => {
+        should(err).be.null;
+      });
+    });
+
+    it('should return json contents by converting received csv', () => {
+      lambdaLocal.execute({
+        event: {
+          content: [
+            'name,email',
+            'john,john@example.com',
+            'jane,jane@example.com',
+            'bob ross,bob@example.com'
+          ].join('\n'),
+          type: 'csv'
+        },
+        lambdaPath: path.join(__dirname, '../index.js'),
+        profileName: 'default',
+        timeoutMs: 3000
+      }).then((done) => {
+        should(done).deepEqual([
+          { "name": "john", "email": "john@example.com" },
+          { "name": "jane", "email": "jane@example.com" },
+          { "name": "bob ross", "email": "bob@example.com" }
+        ]);
+      });
+    });
+
+    it('should return csv contents by converting received json', () => {
+      lambdaLocal.execute({
+        event: {
+          content: [
+            { "name": "john", "email": "john@example.com" },
+            { "name": "jane", "email": "jane@example.com" },
+            { "name": "bob ross", "email": "bob@example.com" }
+          ],
+          type: 'json'
+        },
+        lambdaPath: path.join(__dirname, '../index.js'),
+        profileName: 'default',
+        timeoutMs: 3000
+      }).then((done) => {
+        should(done).eql([
+          'name,email',
+          'john,john@example.com',
+          'jane,jane@example.com',
+          'bob ross,bob@example.com'
+        ].join('\n'));
+      });
+    });
+
+    it('should return csv with a ; delimiter contents by converting received json', () => {
+      lambdaLocal.execute({
+        event: {
+          content: [
+            { "name": "john", "email": "john@example.com" },
+            { "name": "jane", "email": "jane@example.com" },
+            { "name": "bob ross", "email": "bob@example.com" }
+          ],
+          type: 'json',
+          options: {
+            delimiter: {
+              field: ';',
+              array: ','
+            }
+          }
+        },
+        lambdaPath: path.join(__dirname, '../index.js'),
+        profileName: 'default',
+        timeoutMs: 3000
+      }).then((done) => {
+        should(done).eql([
+          'name;email',
+          'john;john@example.com',
+          'jane;jane@example.com',
+          'bob ross;bob@example.com'
+        ].join('\n'));
       });
     });
   });
